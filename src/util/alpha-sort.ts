3a8b509c0b933110d81e5313aa3f8c17e9659dbf
Rodrigo Gonçalves <rodrigo@customizesolucoes.com.br>
2020-04-22 15:34:10 -0300

    feat: Criada ordem de prioridades, respeitando escopo de variáveis
    Ordem: Local, Private, Default, Public

--- src/util/alpha-sort.ts
+++ src/util/alpha-sort.ts
@@ -1,32 +1,23 @@
+import findEntireWordIndex from './string-util';
+import { strict } from 'assert';
+
 const arraySort = (lineA: string, lineB: string): number => {
-  const a = lineA.trim();
-  const b = lineB.trim();
+  const aLevel = getPriorityLevel(lineA.trim());
+  const bLevel = getPriorityLevel(lineB.trim());
 
-  if (a.indexOf("default") > -1 && b.indexOf("default") > -1) {
-    return (a < b) ? -1 : 1;
-  } else if (a.indexOf("default") > -1) {
-    return 1;
-  } else if (b.indexOf("default") > -1) {
-    return -1;
+  if (aLevel !== bLevel) {
+    return (aLevel < bLevel) ? -1 : 1;
   }
 
-  if (a && b) {
-    return (a < b) ? -1 : 1;
-  } else if (a && !b) {
-    return -1;
-  } else if (!a && b) {
-    return 1;
-  }
-
-  return 0;
+  return (lineA.trim() < lineB.trim()) ? -1 : 1;
 };
 
 export const customSort = (a: string[]): string[] => {
   const b = [...a].sort(arraySort);
   const preservedNewlines: string[] = a.map((s, idx) => {
-    if (s.trim() === '') {
-      return '';
-    }
+    // if (s.trim() === '') {
+    //   return '';
+    // }
 
     return b.shift() || '';
   });
@@ -35,3 +26,20 @@ export const customSort = (a: string[]): string[] => {
 };
 
 export default customSort;
+
+const getPriorityLevel = (s: string): Number => {
+  let priorityWords: any = {
+    'local': { level: 0 },
+    'private': { level: 1 },
+    'default': { level: 2 },
+    'public': { level: 3 }
+  };
+
+  for (let word in priorityWords) {
+    if (findEntireWordIndex(s, word) > -1) {
+      return priorityWords[word].level;
+    }
+  }
+
+  return 999;
+};
